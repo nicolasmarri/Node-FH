cfcbed75bff140020b6329807a2002d46754e601
Nicolas Marri <nicomarri22@gmail.com>
2021-09-21 20:10:18 -0300

    template string

---
+++ 02-Fundamentos-Js/template-string.js
@@ -0,0 +1,9 @@
+const nombre = 'Nicolas';
+const apellido = 'Marri';
+
+const datosNombre = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(datosNombre)
+
+console.log(nombreCompleto)
